0c0a4b5d3b0f8e78fcd3ec406e2f35e53b0379c1
Ignacio Pozo <ipozow@gmail.com>
2018-12-25 15:45:04 -0300

    Mejoro interfaz y quito código del dinero

--- Capitalist World iOS/Comprar.swift
+++ Capitalist World iOS/Comprar.swift
@@ -14,27 +14,26 @@ class Comprar: UIViewController {
     //gestos para volver al ViewController
     
     //
-    @IBOutlet weak var cantidadTextField: NSLayoutConstraint!
-   /* var númeroCantidadTextField: Int
     
-    init() {
-        self.númeroCantidadTextField = cantidadTextField.integerValue
-    }*/
-    
-    @IBOutlet weak var collectionView: UICollectionView!
-    //todavía no veo lo de las collectionviews :(
-    //var collectionViewObjects = UICollectionViewCell()
-    
+
     @IBOutlet weak var userAvailableStockLabel: UILabel!
+    
+    @IBAction func textChanged(_ sender: UITextField) {
+        if let cantidad = sender.text {
+            print(cantidad)
+        }
+    }
+    //Por hacer: guardar el valor de cantidad en una variable para luego usarla en el botón de compra
+    @IBAction func textFieldTouchUpOutside(_ sender: UITextField) {
+        if let cantidad = sender.text {
+            print(cantidad)
+        }
+    }
+
     
     var shopAvailableStock = AvailableStock(apples: 500, appleSeeds: 100)
     var shopAvailableStockPrices = StockPrice(applesPrice: 20, appleSeedsPrice: 5)
     
-    @IBAction func buyApplesButton(_ sender: UIButton) {
-        userAvailableStockLabel.text = userAvailableStockLabel.text! + String(/*por ahora puse 10 como cantidad por defecto*/10/*númeroCantidadTextField*/)
-        shopAvailableStock.apples -= 10/*númeroCantidadTextField*/
-        dinero -= shopAvailableStockPrices.applesPrice * 10//númeroCantidadTextField
-    }
-    
+   
     
 }
--- Capitalist World iOS/ViewController.swift
+++ Capitalist World iOS/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-var dinero = 100 /*: Int {
-    UserDefaults.standard.set(dinero, forKey: "dineroActual")
-    return dinero
-}*/
+//
+//var dinero = 100
+///*: Int {
+//    UserDefaults.standard.set(dinero, forKey: "dineroActual")
+//    return dinero
+//}*/
 
 class ViewController: UIViewController {
     
@@ -21,19 +23,9 @@ class ViewController: UIViewController {
     }
     
     @IBAction func venderTransitionButton(_ sender: UIButton) {
-        updateMoney()
     }
+    
     @IBOutlet weak var dineroLabel: UILabel!
     @IBOutlet weak var revenueLabel: UILabel!
-    
-    var profit: Double {
-        if self.profit < 0 {
-            dineroLabel.textColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
-        }
-        return self.profit
-    }
-    func updateMoney() {
-        dineroLabel.text = String(dinero)
-    }
     
 }
