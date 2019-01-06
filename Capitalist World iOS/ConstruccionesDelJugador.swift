750f819aff8ff00f896e006f7e08220250b28455
Ignacio Pozo <ipozow@gmail.com>
2019-01-06 12:07:52 -0300

    agrego pickerview

---
+++ Capitalist World iOS/ConstruccionesDelJugador.swift
@@ -0,0 +1,31 @@
+//
+//  ConstruccionesDelJugador.swift
+//  Capitalist World iOS
+//
+//  Created by Ignacio Pozo Wandersleben on 1/6/19.
+//  Copyright © 2019 Pozo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConstruccionesDelJugador: UIViewController {
+    var tiendas = [""]
+    @IBAction func seleccionarButtonTapped(_ sender: UIButton) {
+    }
+}
+
+extension ConstruccionesDelJugador: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tiendas.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return tiendas[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //al seleccionar la construcción, cambiar de vista
+    }
+}
