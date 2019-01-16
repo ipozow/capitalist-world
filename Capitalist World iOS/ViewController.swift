cd88d86daa4ef13fa8cfacf43c4a16d2d2b7249f
Ignacio Pozo <ipozow@gmail.com>
2019-01-16 16:57:00 -0300

    mejoras de diseño, más orden en el código

--- Capitalist World iOS/ViewController.swift
+++ Capitalist World iOS/ViewController.swift
@@ -27,14 +27,6 @@ func saveDinero() {
 
 class ViewController: UIViewController {
     
-    //let defaults = UserDefaults.standard
-    
-//    struct Keys {
-//        static let totalBalance = "totalBalance"
-//    }
-   
-    //var dinero = 100
-    
     @IBAction func testButton(_ sender: UIButton) {
         dinero += 10
         saveDinero()
@@ -45,12 +37,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         checkSavedDinero()
-        dineroLabel.layer.cornerRadius = 8
-        dineroLabel.layer.masksToBounds = true;
-        revenueLabel.layer.cornerRadius = 8
-        revenueLabel.layer.masksToBounds = true;
-        másEstadísticasLabel.layer.cornerRadius = 8
-        másEstadísticasLabel.layer.masksToBounds = true;
+        revenueLabel.labelStyle()
+        másEstadísticasLabel.labelStyle()
+        dineroLabel.labelStyle()
+        administrarConstruccionesButton.buttonStyle()
+        construirButton.buttonStyle()
+        exportarButton.buttonStyle()
+        importarButton.buttonStyle()
     }
     
     @IBAction func venderTransitionButton(_ sender: UIButton) {
@@ -60,9 +53,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var revenueLabel: UILabel!
     @IBOutlet weak var másEstadísticasLabel: UILabel!
     
+    //botones importar, exportar, construir, administrar construcciones
+    @IBOutlet weak var administrarConstruccionesButton: UIButton!
+    @IBOutlet weak var construirButton: UIButton!
+    @IBOutlet weak var exportarButton: UIButton!
+    @IBOutlet weak var importarButton: UIButton!
+    
     func checkSavedDinero() {
         
         dineroLabel.text = "\(dinero)"
     }
     
+}
+
+extension UILabel {
+    func labelStyle() {
+        self.layer.masksToBounds = true;
+        self.layer.cornerRadius = 12
+        self.layer.shadowRadius = 2.0
+        self.layer.shadowOpacity = 0.2
+        self.layer.shadowOffset = CGSize(width: 1, height: 2)
+    }
+    
+    static func createCustomLabel() -> UILabel {
+        let label = UILabel()
+        label.labelStyle()
+        return label
+    }
+}
+
+extension UIButton {
+    func buttonStyle() {
+        self.titleLabel?.textAlignment = .center
+        self.layer.masksToBounds = true;
+        self.layer.cornerRadius = 12
+    }
+    
+    static func createCustomButton() -> UIButton {
+        let button = UIButton()
+        button.buttonStyle()
+        return button
+    }
 }
