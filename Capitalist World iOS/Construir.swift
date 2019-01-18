999b6fde3545ef932f5bfca2ff6c17ae849455d3
Ignacio Pozo <ipozow@gmail.com>
2019-01-18 13:48:50 -0300

    Preparo construir

--- Capitalist World iOS/Construir.swift
+++ Capitalist World iOS/Construir.swift
@@ -9,6 +9,27 @@
 import Foundation
 import UIKit
 
-class Construir: UIViewController {
+class Construir: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
+    @IBOutlet weak var estructurasCollectionView: UICollectionView!
+    
+    let estructuras = ["Tienda", "Granja", "Fábrica", "Casa", "Departamento", "Centro comercial", "Universidad", "Extracción de recursos naturales", "por definir"]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return estructuras.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ConstruirCollectionView
+        cell.estructuraButton.setTitle("\(estructuras[indexPath.item])", for: .normal)
+        switch estructuras {
+        case ["Tienda"]:
+            let precio = 10000
+        default:
+            <#code#>
+        }
+        cell.layer.masksToBounds = true;
+        cell.layer.cornerRadius = 12
+        return cell
+    }
 }
