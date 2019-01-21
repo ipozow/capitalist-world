08cc3cda45660eff2d80850db1c4afd17613aae1
Ignacio Pozo <ipozow@gmail.com>
2019-01-21 11:15:55 -0300

    precios estructuras

--- Capitalist World iOS/Construir.swift
+++ Capitalist World iOS/Construir.swift
@@ -22,14 +22,44 @@ class Construir: UIViewController, UICollectionViewDelegate, UICollectionViewDat
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ConstruirCollectionView
         cell.estructuraButton.setTitle("\(estructuras[indexPath.item])", for: .normal)
-        switch estructuras {
-        case ["Tienda"]:
-            let precio = 10000
-        default:
-            <#code#>
+        cell.estructuraButton.titleLabel?.minimumScaleFactor = 0.5
+        cell.estructuraButton.titleLabel?.numberOfLines = 0
+        cell.estructuraButton.titleLabel?.adjustsFontSizeToFitWidth = true //por hacer: esto
+        switch estructuras[indexPath.item] {
+            case "Tienda":
+                cell.costoLabel.text = "$ \(10000)"
+                cell.manutenciónLabel.text = "$ \(10000)"
+            case "Granja":
+                cell.costoLabel.text = "$ \(20000)"
+                cell.manutenciónLabel.text = "$ \(20000)"
+                        case estructuras[2]:
+                            cell.costoLabel.text = "$ \(40000)"
+                            cell.manutenciónLabel.text = "$ \(40000)"
+                        case estructuras[3]:
+                            cell.costoLabel.text = "$ \(5000)"
+                            cell.manutenciónLabel.text = "$ \(100)"
+                        case estructuras[4]:
+                            cell.costoLabel.text = "$ \(10000)"
+                            cell.manutenciónLabel.text = "$ \(500)"
+                        case estructuras[5]:
+                            cell.costoLabel.text = "$ \(100000)"
+                            cell.manutenciónLabel.text = "$ \(100000)"
+                        case estructuras[6]:
+                            cell.costoLabel.text = "$ \(50000)"
+                            cell.manutenciónLabel.text = "$ \(10000)"
+                        case estructuras[7]:
+                            cell.costoLabel.text = "$ \(200000)"
+                            cell.manutenciónLabel.text = "$ \(100000)"
+                        case estructuras[8]:
+                            cell.costoLabel.text = "$ \(10000)"
+                            cell.manutenciónLabel.text = "$ \(10000)"
+            default:
+                break
         }
+        
         cell.layer.masksToBounds = true;
         cell.layer.cornerRadius = 12
         return cell
     }
+
 }
