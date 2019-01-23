ddd5cc2789084a6a75b8cf1f175a6466afa63d61
Julián Arias Maetschl <julian.arias@bci.cl>
2019-01-23 10:19:21 -0300

    Agrega arreglo de estructuras

--- Capitalist World iOS/Construir.swift
+++ Capitalist World iOS/Construir.swift
@@ -9,6 +9,11 @@
 import Foundation
 import UIKit
 
+struct Estructura {
+    var nombre: String
+    var precio: Int
+}
+
 class Construir: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var estructurasCollectionView: UICollectionView!
@@ -16,7 +21,9 @@ class Construir: UIViewController, UICollectionViewDelegate, UICollectionViewDat
         createNewView()
     }
     
-    let estructuras = ["Tienda", "Granja", "Fábrica", "Casa", "Departamento", "Centro comercial", "Universidad", "Extracción de recursos naturales", "por definir"]
+    let estructuras: [Estructura] = [Estructura(nombre: "Tienda", precio: 10000),
+                                     Estructura(nombre: "Granja", precio: 20000),
+                                     Estructura(nombre: "Fábrica", precio: 40000)] //, "Casa", "Departamento", "Centro comercial", "Universidad", "Extracción de recursos naturales", "por definir"]
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return estructuras.count
@@ -54,41 +61,12 @@ class Construir: UIViewController, UICollectionViewDelegate, UICollectionViewDat
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ConstruirCollectionView
-        cell.estructuraButton.setTitle("\(estructuras[indexPath.item])", for: .normal)
+        cell.estructuraButton.setTitle("\(estructuras[indexPath.item].nombre)", for: .normal)
         cell.estructuraButton.titleLabel?.minimumScaleFactor = 0.5
         cell.estructuraButton.titleLabel?.numberOfLines = 0
         cell.estructuraButton.titleLabel?.adjustsFontSizeToFitWidth = true //por hacer: esto
-        switch estructuras[indexPath.item] {
-            case "Tienda":
-                cell.costoLabel.text = "$ \(10000)"
-                cell.manutenciónLabel.text = "$ \(10000)"
-            case "Granja":
-                cell.costoLabel.text = "$ \(20000)"
-                cell.manutenciónLabel.text = "$ \(20000)"
-            case estructuras[2]:
-                cell.costoLabel.text = "$ \(40000)"
-                cell.manutenciónLabel.text = "$ \(40000)"
-            case estructuras[3]:
-                cell.costoLabel.text = "$ \(5000)"
-                cell.manutenciónLabel.text = "$ \(100)"
-            case estructuras[4]:
-                cell.costoLabel.text = "$ \(10000)"
-                cell.manutenciónLabel.text = "$ \(500)"
-            case estructuras[5]:
-                cell.costoLabel.text = "$ \(100000)"
-                cell.manutenciónLabel.text = "$ \(100000)"
-            case estructuras[6]:
-                cell.costoLabel.text = "$ \(50000)"
-                cell.manutenciónLabel.text = "$ \(10000)"
-            case estructuras[7]:
-                cell.costoLabel.text = "$ \(200000)"
-                cell.manutenciónLabel.text = "$ \(100000)"
-            case estructuras[8]:
-                cell.costoLabel.text = "$ \(10000)"
-                cell.manutenciónLabel.text = "$ \(10000)"
-            default:
-                break
-        }
+        cell.costoLabel.text = "$ \(estructuras[indexPath.row].precio)"
+        cell.manutenciónLabel.text = "$ \(estructuras[indexPath.row].precio)"
         
         cell.layer.masksToBounds = true;
         cell.layer.cornerRadius = 12
