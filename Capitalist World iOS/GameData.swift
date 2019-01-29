a03825bfc324f566c019d4df821974eea64ef7ea
Julián Arias Maetschl <julian.arias@bci.cl>
2019-01-29 11:09:16 -0300

    Agrega estructura propuesta

--- Capitalist World iOS/GameData.swift
+++ Capitalist World iOS/GameData.swift
@@ -9,8 +9,30 @@
 import Foundation
 
 struct GameData {
-    var dinero: Int
-    var stock: StockData
-    var tienda: [Tienda]
+    var userInfo: UserInfo
     var lastUpdate: Date
+    var balance: Double
+    var structures: [Structure] // Todas las estructuras
+}
+
+struct UserInfo {
+    var userName: String
+    var userId: String
+}
+
+protocol Structure { // Esto es lo que tienen que tener las estructuras para que el jeugo funcione
+    // Pondré unos ejemplos que me parecen bien
+    var structureType: StructureType { get set } // Tipo de estructura
+    var name: String  { get set } // El nombre de la estructura
+    func incomePerMinute() -> Double // El oro que dará por minuto
+    func onDestroy() // Por ejemplo, que pasa cuando se detruye
+    func upgrade() // Por ejemplo, un método para subirlo de nivel
+}
+
+
+enum StructureType: String {
+    case fabrica = "Fábrica"
+    case granga = "Granja"
+    case universidad = "Universidad"
+    case etc = "Etc..."
 }
